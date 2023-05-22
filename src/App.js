1ad2538ab1f5f8c16ca3871507892d4a0e6f5a6c
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-05-22 08:36:38 +0100

    debugging

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
-import { Route, Routes } from "react-router-dom";
-import SignUp from "./SignUp";
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <Routes>
-        <Route path="/register" element={<SignUp/>}/>
-      </Routes>
     </div>
   );
 };
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,63 +7,63 @@ import Home from "../LandingPage/Home";
 import Pricing from "../LandingPage/Pricing";
 import Blog from "../LandingPage/Blog";
 
+const Navbar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px 5%;
+  font-size: 16px;
+  font-family: "Urbanist";
+`;
+
+const LogoNavContainer = styled.div`
+  display: flex;
+  gap: 1em;
+  align-items: center;
+  width: 70%;
+`;
+
+const Nav = styled(NavLink)`
+  color: #00191f;
+`;
+
+const List = styled.ul`
+  display: flex;
+  list-style-type: none;
+  gap: 1em;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Register = styled(Link)`
+  border: none;
+  background: transparent;
+  color: #00191f;
+  font-size: 1.1em;
+  text-decoration: none;
+  height: fit-content;
+  padding: 15px 30px;
+`;
+
+const Login = styled(Link)`
+  background: #007e99;
+  border: 0;
+  color: white;
+  font-size: 1em;
+  height: fit-content;
+  padding: 15px 40px;
+  border-radius: 5px;
+  text-decoration: none;
+  &:hover {
+    background-color: #006980;
+    transition: ease-in-out 0.3s;
+  }
+`;
+
 const NavBar = () => {
-  const Navbar = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 24px 5%;
-    font-size: 16px;
-    font-family: "Urbanist"
-  `;
-
-  const LogoNavContainer = styled.div`
-    display: flex;
-    gap: 1em;
-    align-items: center;
-    width: 70%;
-  `;
-
-  const Nav = styled(NavLink)`
-    color: #00191f;
-  `;
-
-  const List = styled.ul`
-    display: flex;
-    list-style-type: none;
-    gap: 1em;
-  `;
-
-  const ButtonsContainer = styled.div`
-    display: flex;
-    align-items: center;
-  `;
-
-  const Register = styled(Link)`
-    border: none;
-    background: transparent;
-    color: #00191f;
-    font-size: 1.1em;
-    text-decoration: none;
-    height: fit-content;
-    padding: 15px 30px;
-  `;
-
-  const Login = styled(Link)`
-    background: #007e99;
-    border: 0;
-    color: white;
-    font-size: 1em;
-    height: fit-content;
-    padding: 15px 40px;
-    border-radius: 5px;
-    text-decoration: none;
-    &:hover {
-      background-color: #006980;
-      transition: ease-in-out 0.3s;
-    }
-  `;
-
   return (
     <>
       <Navbar>
@@ -112,7 +112,7 @@ const NavBar = () => {
           </List>
         </LogoNavContainer>
         <ButtonsContainer>
-          <Register to="/register">Register</Register>
+          <Register>Register</Register>
           <Login>Login</Login>
         </ButtonsContainer>
       </Navbar>

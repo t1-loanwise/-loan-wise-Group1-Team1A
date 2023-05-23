20487af2fe71bc4974720cd04afcd99c14347f2c
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-05-23 21:12:36 +0100

    feat: Created set security page file/updated routes in app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/LandingPage/Home";
 import Pricing from "./pages/LandingPage/Pricing";
 import Blog from "./pages/LandingPage/Blog";
 import SignUp from "./pages/SignUp";
+import SetSecurityQuestion from "./pages/SetSecurityQuestion";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/register" element={<SignUp />} />
+        <Route path="/set-security-questions" element={<SetSecurityQuestion />} />
       </Routes>
     </div>
   );
---
+++ src/pages/SetSecurityQuestion.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {AuthenticationMainText} from "../components/AuthenticationMainText"
+
+const SetSecurityQuestion = () => {
+  return (
+    <div>
+      <div>
+
+      </div>
+      <div>
+        <AuthenticationMainText
+          Title="Set Security Question"
+          Body="To keep your account secured, create 2 security questions"
+        />
+
+      </div>
+    </div>
+  );
+}
+
+export default SetSecurityQuestion

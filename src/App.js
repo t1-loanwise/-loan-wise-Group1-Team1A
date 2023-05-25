a0e034345d7b56aab1fa669f0e14121f71f16af6
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-05-25 22:45:20 +0100

    feat: Created a verify email page for signUp

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Pricing from "./pages/LandingPage/Pricing";
 import Blog from "./pages/LandingPage/Blog";
 import SignUp from "./pages/AuthenticationPage/CreateAccount/SignUp";
 import SetSecurityQuestion from "./pages/AuthenticationPage/CreateAccount/SetSecurityQuestion";
+import VerifyRegistration from "./pages/AuthenticationPage/CreateAccount/VerifyRegistration";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/blog" element={<Blog />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/securityQuestions" element={<SetSecurityQuestion />}/>
+        <Route path="/verifyRegistration" element={<VerifyRegistration/>}/>
       </Routes>
     </div>
   );

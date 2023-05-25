c4edeaa40ea15a5883c25a8399e7d63f3d2f8079
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-05-25 23:36:06 +0100

    feat: separated the newsletter section from the footer component

--- src/pages/LandingPage/Home.js
+++ src/pages/LandingPage/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../../components/Footer";
 import NavBar from "../../components/NavBar";
+import Newsletter from "../../components/Newsletter";
 
 const Home = () => {
   return (
     <>
       <NavBar />
+      <Newsletter/>
       <Footer />
     </>
   );

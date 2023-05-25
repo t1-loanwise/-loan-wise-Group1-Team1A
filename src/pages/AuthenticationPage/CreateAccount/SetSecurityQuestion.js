59972bf72d28b37d5b4f91ba6c5f9baf75fa57ae
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-05-25 08:37:28 +0100

    feat: updated routes in App.js

--- src/pages/AuthenticationPage/CreateAccount/SetSecurityQuestion.js
+++ src/pages/AuthenticationPage/CreateAccount/SetSecurityQuestion.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { AuthenticationMainText } from "../../../components/AuthenticationMainText";
+import AuthenticationImage from "../../../components/AuthenticationImage"
+import Logo from "../../../components/Logo"
 
 const SetSecurityQuestion = () => {
   return (
     <div>
-      <div></div>
+      <AuthenticationImage/>
       <div>
+        <Logo/>
         <AuthenticationMainText
           Title="Set Security Question"
           Body="To keep your account secured, create 2 security questions"

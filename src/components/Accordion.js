5a5bad8dd7b69e94a4331b34f482d02c8a42a939
muftiatbakare <bakare.muftiat@gmail.com>
2023-06-03 04:19:54 +0100

    update: style pricing page to pixel perfect

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,32 +2,44 @@ import React, { useState } from "react";
 import plus from "../assets/icons/plus.png";
 import { IconContext } from "react-icons";
 import { BiMinusCircle } from "react-icons/bi";
+import { HiMinusCircle, HiPlusCircle } from "react-icons/hi";
+// import { FaCirclePlus } from "react-icons/fa";
 
 const Accordion = ({ content, title }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isHover, setIsHover] = useState(false);
   const handleClick = () => {
     setIsOpen(!isOpen);
+  };
+  const handleMouseOver = () => {
+    setIsHover(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHover(false);
   };
   return (
     <>
       <div className="faq-accordion">
-        <div className="faq-question" onClick={handleClick}>
+        <div
+          className="faq-question"
+          onMouseEnter={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
+          onClick={handleClick}
+        >
           {title}
           {isOpen ? (
             <IconContext.Provider value={{ className: "minus" }}>
-              <div>
-                <BiMinusCircle />
-              </div>
+              <div>{isHover ? <HiMinusCircle /> : <BiMinusCircle />}</div>
             </IconContext.Provider>
           ) : (
             <div>
-              <img
-                src={plus}
-                height="20"
-                width="20"
-                alt="plus"
-                // className="plus"
-              />
+              {isHover ? (
+                <IconContext.Provider value={{ className: "plus" }}>
+                  <HiPlusCircle />
+                </IconContext.Provider>
+              ) : (
+                <img src={plus} height="20" width="20" alt="plus" />
+              )}
             </div>
           )}
         </div>

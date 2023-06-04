e91a9f8155895288582fd44e182b858238f7a008
muftiatbakare <bakare.muftiat@gmail.com>
2023-06-04 08:42:05 +0100

    feat: create the Progress Bar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/ProgressBar.css";
+
+function ProgressBar() {
+  return (
+    <>
+      <div className="progress_bar">
+        <div className="progress_circle">circle</div>
+      </div>
+    </>
+  );
+}
+
+export default ProgressBar;

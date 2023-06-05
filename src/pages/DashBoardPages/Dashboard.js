cbb445756a39ba907cd43916c46b3238508ba4fe
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-06-05 19:58:20 +0100

    defined styles for pages-content container

--- src/pages/DashBoardPages/Dashboard.js
+++ src/pages/DashBoardPages/Dashboard.js
@@ -3,16 +3,22 @@ import DashCardGraph from '../../components/DashCardGraph'
 
 const Dashboard = () => {
   return (
-    <div style={{width: "80%", justifySelf: "end"}}>
-      <DashCardGraph/>
-      <DashCardGraph/>
-      <DashCardGraph/>
-      <DashCardGraph/>
-      <DashCardGraph/>
-      <DashCardGraph/>
+    <div
+      style={{
+        width: "80%",
+        justifySelf: "end",
+        marginTop: "15vh",
+        padding: "3% 3% 0 0",
+      }}
+    >
+      <DashCardGraph />
       <div>Hello!</div>
+      <div>Hello!</div>
+      <div>Hello!</div>
+      <div>Hello!</div>
+      <DashCardGraph />
     </div>
-  )
+  );
 }
 
 export default Dashboard

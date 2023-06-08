c1a49ae97aac8dc1da7b4fee6cc6b14f10782751
Nancy <olatujamn.19@student.funaab.edu.ng>
2023-06-08 20:58:29 +0100

    Link Back Button

--- src/components/UserPreferenceMain.js
+++ src/components/UserPreferenceMain.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router';
 import UserPreferenceSection from './UserPreferenceSection';
-import back from "../assets/back arrow.svg"
+import back from "../assets/back arrow.svg";
 
 const UserPreferenceMain = () => {
+ const navigate = useNavigate();
+
+ const handleBackClick = () => {
+  navigate('/settings')
+ }
   return (
     <div className=" rightContent">
-      <div id="back-btn">
-        <img src={back} />
-        Back
+      <div className='back-btn-container'>
+         <button id="back-btn" onClick={handleBackClick}>
+        <img src={back} alt='back-button'/>
+        <p>Back</p>
+      </button>
       </div>
       <UserPreferenceSection />
     </div>

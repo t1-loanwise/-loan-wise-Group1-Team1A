e6a7478a5ee94dadf65e5f3e8e66cd1786653cdd
ekeukeni <ukenieke@gmail.com>
2023-06-09 21:54:38 +0100

    updated

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,40 +1,98 @@
-import{ useState } from "react";
+import React, { useState } from "react";
 import '../styles/PortfolioTable.css';
-import UseTable from './Portfolio/useTable';
-import FooterTable from './Portfolio/FooterTable';
-import { Link, NavLink } from "react-router-dom";
+import leftarrow from "../..assets/paginationleftarrow.svg";
+import rightarrow from "./assets/paginationrightarrow.svg";
+import persons from "../components/Portfolio/Data";
 
-const Table = ({ data, rowsPerPage }) => {
-    const [page, setPage] = useState(1);
-    const { slice, range } = UseTable(data, page, rowsPerPage);
+const Table = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6; // Number of items to display per page
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
     return (
       <>
-        <table className="table">
-          <thead className="tableRowHeader">
-            <tr>
-              <th><Link className="tableLinks">ID</Link></th>
-              <th><Link className="tableLinks">Name</Link></th>
-              <th><Link className="tableLinks">Category</Link></th>
-              <th><Link className="tableLinks">Amount</Link></th>
-              <th><Link className="tableLinks">Due Date</Link></th>
-              <th><Link className="tableLinks">Status</Link></th>
-            </tr>
+        <table>
+          <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Amount</th>
+            <th>Due Date</th>
+            <th>Status</th>
           </thead>
+  
           <tbody>
-            {slice.map((el) => (
-              <tr className="tableRowItems" key={el.id}>
-                <td className="tableCell"><NavLink className="tableLinks">{el.id}</NavLink></td>
-                <td className="tableCell"><NavLink className="tableLinks">{el.Name}</NavLink></td>
-                <td className="tableCell"><NavLink className="tableLinks">{el.Category}</NavLink></td>
-                <td className="tableCell"><NavLink className="tableLinks">{el.Amount}</NavLink></td>
-                <td className="tableCell"><NavLink className="tableLinks">{el.DueDate}</NavLink></td>
-                <td className={el.Status}><NavLink className="tableLinks"><button>{el.Status}</button></NavLink></td>
-              </tr>
-            ))}
-           
+            {persons
+              .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+              .map((data, index) => (
+                <tr key={index}>
+                  <td>{data.ID}</td>
+                  <td>{data.Name}</td>
+                  <td>{data.Category}</td>
+                  <td>{data.Amount}</td>
+                  <td>{data.DueDate}</td>
+                  <td className={data.Status}>
+                    <button>{data.Status}</button>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
-        <FooterTable className="pagination" range={range} slice={slice} setPage={setPage} page={page} />
+  
+        <div className="lowerlastdash">
+          <a className="loanhiistory" href="">View all loan history</a>
+          <div className="pagiNumbs">
+            <img src={leftarrow}
+              onClick={() => handlePageChange(currentPage - 1)}
+            />
+  
+            {currentPage > 6 && <p>...</p>}
+  
+            {Array.from(
+              { length: Math.ceil(persons.length / itemsPerPage) },
+              (_, i) => {
+                if (i + 1 > currentPage + 2) return null;
+                if (i + 1 >= currentPage - 2) {
+                  return (
+                    <button
+                      key={i}
+                      className={currentPage === i + 1 ? "active" : ""}
+                      onClick={() => handlePageChange(i + 1)}
+                    >
+                      {i + 1}
+                    </button>
+                  );
+                }
+                return null;
+              }
+            )}
+  
+            {currentPage < Math.ceil(persons.length / itemsPerPage) - 4 && (
+              <p>...</p>
+            )}
+  
+            <button
+              className={
+                currentPage === Math.ceil(persons.length / itemsPerPage)
+                  ? "active"
+                  : ""
+              }
+              onClick={() =>
+                handlePageChange(Math.ceil(persons.length / itemsPerPage))
+              }
+            >
+              {Math.ceil(persons.length / itemsPerPage)}
+            </button>
+  
+            <img
+              src={rightarrow}
+              onClick={() => handlePageChange(currentPage + 1)}
+            />
+          </div>
+        </div>
       </>
     );
 }

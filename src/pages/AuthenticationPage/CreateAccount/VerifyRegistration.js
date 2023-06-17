e744fb0910b06365abab1d137cbcfc50f9a94341
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-06-17 19:02:51 +0100

    added loanwiseData file for everyone's use

--- src/pages/AuthenticationPage/CreateAccount/VerifyRegistration.js
+++ src/pages/AuthenticationPage/CreateAccount/VerifyRegistration.js
@@ -3,6 +3,7 @@ import AuthenticationMainText from "../../../components/AuthenticationMainText";
 import Logo from "../../../components/Logo";
 import Onboarding from "../../../components/Onboarding";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const VerifyRegistration = () => {
   const [otp, setOtp] = useState(["", "", "", ""]);
@@ -10,7 +11,10 @@ const VerifyRegistration = () => {
   const [timer, setTimer] = useState(60);
   const [disabled, setDisabled] = useState(false);
   const [countdownFinished, setCountdownFinished] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [token, setToken] = useState(null);
+  const [error, setError] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const handleOtpChange = (event, index) => {
     const value = event.target.value;
     if (!isNaN(value) || value === "") {
@@ -57,16 +61,32 @@ const VerifyRegistration = () => {
     setCountdownFinished(false);
   };
 
-  const handleVerify = () => {
+  const handleVerify = async (e) => {
+    e.preventDefault();
     const isOtpFilled = otp.every((digit) => digit !== "");
     if (isOtpFilled) {
       const enteredOtp = otp.join("");
-      // Perform verification logic here with enteredOtp
-      console.log("Entered OTP:", enteredOtp);
-      navigate("/securityQuestions");
-    } else {
-      // Show an error message or perform some action if all input fields are not filled
-      console.log("Please fill all OTP fields.");
+      const email = localStorage.getItem("email")
+      try {
+        setIsSubmitting(true);
+        const response = await axios.post(
+          `https://loanwise.onrender.com/api/verify-signup`,
+          {
+            email: email,
+            verificationCode: enteredOtp,
+          }
+        );
+        setToken(response.data);
+        setError(false);
+        setIsSubmitting(false);
+        navigate("/securityQuestions");
+        console.log("Entered OTP:", enteredOtp);
+        console.log(response.data);
+      } catch (error) {
+        setError(true);
+        setIsSubmitting(false);
+       setOtp(["","","",""])
+      }
     }
   };
 
@@ -83,29 +103,39 @@ const VerifyRegistration = () => {
             Body="Thank you for signing up, please enter the verification code we sent to your email address @johndoe@gmail.com"
           />
           <form onSubmit={handleVerify}>
-            <div className="verifyInput">
-              {otp.map((digit, index) => (
-                <input
-                  key={index}
-                  type="text"
-                  maxLength={1}
-                  value={digit}
-                  onChange={(event) => handleOtpChange(event, index)}
-                  onKeyDown={(event) => handleKeyDown(event, index)}
-                  ref={(ref) => (inputRefs.current[index] = ref)}
-                  required
-                />
-              ))}
+            <div className="input-error-container">
+              {error && (
+                <span className="invalid-token">Invalid Token !!!</span>
+              )}
+              <div className="verifyInput">
+                {otp.map((digit, index) => (
+                  <input
+                    key={index}
+                    type="text"
+                    maxLength={1}
+                    value={digit}
+                    onChange={(event) => handleOtpChange(event, index)}
+                    onKeyDown={(event) => handleKeyDown(event, index)}
+                    ref={(ref) => (inputRefs.current[index] = ref)}
+                    required
+                  />
+                ))}
+              </div>
             </div>
-            <button className="createAccountBtn">Verify</button>
+            <button className="createAccountBtn">
+              {isSubmitting ? (
+                <i className="fa fa-circle-o-notch fa-spin"></i>
+              ) : (
+                "Verify"
+              )}
+            </button>
           </form>
         </div>
-       
+
         <span className="no_OTP">
           Didn't get an OTP?
           {countdownFinished ? (
             <span onClick={handleResend} className="resend_OTP">
-              {" "}
               Resend
             </span>
           ) : (

50e1638e63c97681454ada896456997024ee6cf1
muftiatbakare <bakare.muftiat@gmail.com>
2023-06-24 14:27:41 +0100

    make chages to show new design and fill progress bar

--- src/components/NewPortfolio/NewPredictionResult.js
+++ src/components/NewPortfolio/NewPredictionResult.js
@@ -3,18 +3,7 @@ import SuccessReport from "./SuccessReport";
 import UnsuccessfulReport from "./UnsuccessfulReport";
 
 function NewPredictionResult() {
-<<<<<<< HEAD
-  let creditScore = 70;
-  const showPrediction = () => {
-    if (creditScore > 50) {
-      <SuccessReport />;
-    }
-    <UnsuccessfulReport />;
-  };
-  return <>{showPrediction()}</>;
-=======
   return <div className="result-analysis">NewPredictionResult</div>;
->>>>>>> b28653029af4626943f0625170cb8bccdcf0cf89
 }
 
 export default NewPredictionResult;
